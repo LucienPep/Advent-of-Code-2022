ba8a0fb1d2f3f27733ed32bf8824c22e79ca8b2b
LucienPep <xxyyzzoozz@gmail.com>
2023-08-16 15:43:10 +0100

    Optimised Doy03/ Started Day04

--- 03/Day03.py
+++ 03/Day03.py
@@ -2,12 +2,12 @@
 #Part 1
 with open("RucksackData.txt", "r") as f:
     sackData = f.readlines()
-    commonElements = []
 
-    for line in sackData:
-        first = line[: len(line) // 2]
-        second = line[len(line) // 2 :]
-        commonElements += set(first).intersection(second)
+commonElements = []
+for line in sackData:
+    first = line[: len(line) // 2]
+    second = line[len(line) // 2 :]
+    commonElements += set(first).intersection(second)
     
 totalValue = 0
 for letter in commonElements:
@@ -17,17 +17,15 @@ print(totalValue)
 
 #Part 2
 commonElements = []
-for line in sackData:
-    i = sackData.index(line) - 1
-    if i % 3 == 0:
-        commonElements += set(sackData[i]).intersection(sackData[i + 1], sackData[i + 2])
-        
+for i in range(0, len(sackData), 3):
+    commonElements += set(sackData[i]).intersection(sackData[i + 1], sackData[i + 2])
+
 totalValue = 0
 for letter in commonElements:
     i = ord(letter)
     if i != 10:
         totalValue += i - (96 if letter.islower() else 38)
-        
+       
 print(totalValue)
             
             
---
+++ 04/Day04.py
@@ -0,0 +1,7 @@
+#Advent Day 04
+#Part 1
+with open("cleaningInput.txt", "r") as f:
+    cleaningData = f.readlines()
+
+for line in cleaningData:    
+    print(line)
