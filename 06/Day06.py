0d1e0541f22bd90be49c49e29805a80f686cfd7f
LucienPep <xxyyzzoozz@gmail.com>
2023-08-19 12:09:12 +0100

    Completed Daoy06/ needs optimising

--- 06/Day06.py
+++ 06/Day06.py
@@ -1,7 +1,27 @@
-#Advent Day 05
-import copy
+#Advent Day 06
 with open("SignalInput.txt") as f:
     signalData = f.read()
 
-for i in signalData:
-    print(i)
+for i in range(0, len(signalData)):
+    tempList = []
+    tempSet = set()
+    for x in range(0, 4):
+        tempList.append(signalData[i + x])
+    answer = any(char in tempSet or tempSet.add(char) for char in tempList)
+    
+    if answer == False:
+        print(tempList)
+        print(i + 4)
+        break
+
+for i in range(0, len(signalData)):
+    tempList = []
+    tempSet = set()
+    for x in range(0, 14):
+        tempList.append(signalData[i + x])
+    answer = any(char in tempSet or tempSet.add(char) for char in tempList)
+    
+    if answer == False:
+        print(tempList)
+        print(i + 14)
+        break
